66eb90d55edb242dfd1649f95f97018a3cd0a4cf
P4lbL0 <lemire.angelos@gmail.com>
2025-11-19 10:15:16 +0100

    version finale 1

---
+++ js/data2.js
@@ -0,0 +1,23 @@
+export const statWeeks = ["S13", "S14", "S15"];
+
+export const statData = [
+    { name: "Dorian",   kills: [null, null, null], wins: [null, null, null] },
+    { name: "Angelos",  kills: [null, null, null], wins: [null, null, null] },
+    { name: "Ghost",    kills: [null, null, null], wins: [null, null, null] },
+    { name: "Gaspard",  kills: [null, null, null], wins: [null, null, null] },
+    { name: "Paul",     kills: [null, null, null], wins: [null, null, null] },
+    { name: "Rico",     kills: [null, null, null], wins: [null, null, null] },
+    { name: "Maxence",  kills: [null, null, null], wins: [null, null, null] },
+    { name: "Nevroz",   kills: [null, null, null], wins: [null, null, null] },
+    { name: "Ashtax",   kills: [null, null, null], wins: [null, null, null] },
+    { name: "Landry",   kills: [null, null, null], wins: [null, null, null] },
+    { name: "Zeroday",  kills: [null, null, null], wins: [null, null, null] },
+    { name: "Beya",     kills: [null, null, null], wins: [null, null, null] },
+    { name: "Deadshot", kills: [null, null, null], wins: [null, null, null] },
+    { name: "Zan",      kills: [null, null, null], wins: [null, null, null] },
+    { name: "Oxy",      kills: [null, null, null], wins: [null, null, null] },
+    { name: "Iassonas", kills: [null, null, null], wins: [null, null, null] },
+    { name: "Myem",     kills: [null, null, null], wins: [null, null, null] },
+    { name: "Excentrizc", kills: [null, null, null], wins: [null, null, null] },
+    { name: "Gabriel",  kills: [null, null, null], wins: [null, null, null] },
+];
---
+++ js/data3.js
@@ -0,0 +1,22 @@
+export const weeks = ["S1", "S2", "S3", "S4", "S5", "S6"];
+
+export const rawData = [
+    { name: "Rico",      scores: [33, 45, 38, 58, 40, 22] },
+    { name: "Benji",     scores: [23, 41, 13, null, null, null] },
+    { name: "Paul",      scores: [23, null, 13, 16, 15, 22] },
+    { name: "Ghost",     scores: [22, 17, 22, 28, 18, 11] },
+    { name: "Angelos",   scores: [22, 26, 22, 28, 18, 61] },
+    { name: "Deadshot",  scores: [10, 45, 38, 58, 21, null] },
+    { name: "Zan",       scores: [null, 41, null, null, null, null] },
+    { name: "Iassonas",  scores: [null, 26, null, 16, null, null] },
+    { name: "Oxy",       scores: [null, 17, null, null, 24, null] },
+    { name: "Beya",      scores: [null, 13, null, null, 35, 11] },
+    { name: "Black",     scores: [null, 13, null, null, null, null] },
+    { name: "Excentrizc",scores: [null, null, null, 28, null, null] },
+    { name: "Seong",     scores: [null, null, null, 16, 15, null] },
+    { name: "Gabriel",   scores: [null, null, null, 16, 15, 11] },
+    { name: "Lucas",     scores: [null, null, null, null, 40, null] },
+    { name: "Malo",      scores: [null, null, null, null, 21, null] },
+    { name: "Manu",      scores: [null, null, null, null, 15, 11] },
+    { name: "Angel",     scores: [null, null, null, null, null, 61] },
+];
