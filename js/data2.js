39efe7eed2b2ec7e4e8a2ffbf2fb2673122a44af
P4lbL0 <lemire.angelos@gmail.com>
2025-12-06 23:22:32 +0100

    mjs15

--- js/data2.js
+++ js/data2.js
@@ -1,22 +1,26 @@
 export const detailedData = {
     Rico: {
         S13: { w: 4, svp: 1, k: 36, a: 9 },
-        S14: { w: 4, svp: 1, k: 40, a: 2 }
+        S14: { w: 4, svp: 1, k: 40, a: 2 },
+        S15: { w: 3, svp: 0, k: 39, a: 1 } // NOUVEAU S15
     },
 
     Angelos: {
         S13: { w: 2, svp: 2, k: 51, a: 5 },
-        S14: { w: 3, svp: 2, k: 46, a: 3 }
+        S14: { w: 3, svp: 2, k: 46, a: 3 },
+        S15: { w: 3, svp: 2, k: 55, a: 3 } // NOUVEAU S15
     },
 
     Seveun: {
-        S13: { w: 3, svp: 0, k: 23, a: 6 }
+        S13: { w: 3, svp: 0, k: 23, a: 6 },
         // Rien reçu pour S14 → pas ajouté
+        S15: { w: 3, svp: 1, k: 39, a: 2 } // NOUVEAU S15
     },
 
     Silence: {
         S13: { w: 4, svp: 1, k: 49, a: 6 },
-        S14: { w: 4, svp: 2, k: 61, a: 6 }
+        S14: { w: 4, svp: 2, k: 61, a: 6 },
+        S15: { w: 3, svp: 1, k: 30, a: 7 } // NOUVEAU S15
     },
 
     Iassonas: {
@@ -35,8 +39,9 @@ export const detailedData = {
     },
 
     Gabriel: {
-        S13: { w: 2, svp: 1, k: 23, a: 0 }
+        S13: { w: 2, svp: 1, k: 23, a: 0 },
         // Rien S14
+        S15: { w: 4, svp: 0, k: 16, a: 2 } // NOUVEAU S15
     },
 
     Angel: {
@@ -50,14 +55,20 @@ export const detailedData = {
     },
 
     Paul: {
-        S14: { w: 2, svp: 0, k: 14, a: 0 }
+        S14: { w: 2, svp: 0, k: 14, a: 0 },
+        S15: { w: 2, svp: 0, k: 16, a: 1 } // NOUVEAU S15
     },
 
     Deadshot: {
         S14: { w: 3, svp: 0, k: 25, a: 4 }
     },
 
-    Excentrizc: {
-        S14: { w: 4, svp: 0, k: 19, a: 1 }
+    Excentrizc: { // Clé originale gardée pour S14
+        S14: { w: 4, svp: 0, k: 19, a: 1 },
+        S15: { w: 1, svp: 1, k: 17, a: 2 }
+    },
+
+    Black: { // NOUVEAU JOUEUR
+        S15: { w: 2, svp: 0, k: 17, a: 0 }
     }
 };
