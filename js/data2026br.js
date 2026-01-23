68c7eed2b6a1b647fcf04975c6939f4be3b18043
P4lbL0 <lemire.angelos@gmail.com>
2026-01-23 21:07:51 +0100

    brscoreS3

--- js/data2026br.js
+++ js/data2026br.js
@@ -1,2 +1,122 @@
-export const detailedbrData2026 = {
-};
+export const weeks = ["S1", "S2"];
+export const brData = [
+    {
+        "name": "Seveun",
+        "scores": [
+            72,
+            54
+        ]
+    },
+    {
+        "name": "Venom",
+        "scores": [
+            72,
+            null
+        ]
+    },
+    {
+        "name": "Rico",
+        "scores": [
+            31,
+            37
+        ]
+    },
+    {
+        "name": "Deadshot",
+        "scores": [
+            31,
+            37
+        ]
+    },
+    {
+        "name": "Paul",
+        "scores": [
+            24,
+            17
+        ]
+    },
+    {
+        "name": "Black",
+        "scores": [
+            24,
+            null
+        ]
+    },
+    {
+        "name": "Excentrizc",
+        "scores": [
+            17,
+            24
+        ]
+    },
+    {
+        "name": "Capi",
+        "scores": [
+            17,
+            null
+        ]
+    },
+    {
+        "name": "Seong",
+        "scores": [
+            15,
+            null
+        ]
+    },
+    {
+        "name": "Afro",
+        "scores": [
+            15,
+            null
+        ]
+    },
+    {
+        "name": "Angelos",
+        "scores": [
+            null,
+            61
+        ]
+    },
+    {
+        "name": "Leo",
+        "scores": [
+            null,
+            61
+        ]
+    },
+    {
+        "name": "Ashtax",
+        "scores": [
+            null,
+            54
+        ]
+    },
+    {
+        "name": "Angel",
+        "scores": [
+            null,
+            42
+        ]
+    },
+    {
+        "name": "Karantrwa",
+        "scores": [
+            null,
+            42
+        ]
+    },
+    {
+        "name": "Lucas",
+        "scores": [
+            null,
+            24
+        ]
+    },
+    {
+        "name": "Gabriel",
+        "scores": [
+            null,
+            17
+        ]
+    }
+];
